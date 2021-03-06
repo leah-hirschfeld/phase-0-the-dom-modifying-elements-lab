b4f4f17cd728eb5887b9acc081fe9a2d5ad6e4e0
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-03-06 10:50:06 -0800

    assignment complete
    ;
    exit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1'); 
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+
+const newContent = document.createTextNode("Leah is the champion");
+newHeader.appendChild(newContent);
